e5d3289d2a9e9fb626e2d2fcdc142506a28e023f
burno1 <brunofernandes100@hotmail.com.br>
2020-09-12 21:56:07 -0300

    basic form with some updates on stlyes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BasicFormComponent } from './basic-form/basic-form.component';
+import { NestedFormComponent } from './nested-form/nested-form.component';
+import { SubmitFormComponent } from './submit-form/submit-form.component';
+import { ArrayFormComponent } from './array-form/array-form.component';
+
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule, MatSelectModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicFormComponent,
+    NestedFormComponent,
+    SubmitFormComponent,
+    ArrayFormComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
